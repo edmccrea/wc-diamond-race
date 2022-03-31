70a170d5eeaf426eafb8311a029861ec6e7c0602
edmccrea <ed-mccrea@hotmail.co.uk>
2022-03-31 18:21:16 +0200

    move game over check into own function

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const resetBtn = document.querySelector('.reset-btn');
 
 let isGameOver = false;
 
+//Create function to check for gameover condition
+const checkGameOver = () => {
+  scores.forEach((score) => {
+    const valueCheck = score.shadowRoot.childNodes[5];
+    if (valueCheck.innerHTML === '10') {
+      diamonds.forEach((diamond) => {
+        diamond.deactivate();
+        isGameOver = true;
+      });
+    }
+  });
+};
+
 //Run game on click of diamond
 diamonds.forEach((diamond, i) => {
   diamond.addEventListener('click', () => {
@@ -13,16 +26,7 @@ diamonds.forEach((diamond, i) => {
       diamond.increment();
       scores[i].increment();
 
-      //Check if game over conditions are met
-      scores.forEach((score) => {
-        const valueCheck = score.shadowRoot.childNodes[5];
-        if (valueCheck.innerHTML === '10') {
-          diamonds.forEach((diamond) => {
-            diamond.deactivate();
-            isGameOver = true;
-          });
-        }
-      });
+      checkGameOver();
     }
   });
 });
